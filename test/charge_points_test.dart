43426196937239ed68997e3db4d5a78fe68cb911
Enrico Moschella <enricomoschella99@gmail.com>
2023-08-17 15:05:50 +0200

    fix testing purpose

--- test/charge_points_test.dart
+++ test/charge_points_test.dart
@@ -3,7 +3,7 @@ import 'package:charge_points/src/model/bounding_box.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Model Test', () {
+  group('API Test', () {
     test('Retrive Pois Around Globe', () async {
       final topLeftCorner = Corner(90, -180);
       final bottomRightCorner = Corner(-90, 180);
